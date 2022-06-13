847235424e09cd2e087d302ec4cd2b191b83d91d
Alexandre <almmello@gmail.com>
2022-06-12 21:08:11 -0300

    Exercise OK

--- app.py
+++ app.py
@@ -11,14 +11,25 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+class TodoList(db.Model):
+  __tablename__ = 'todolists'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+  todos = db.relationship('Todo', backref='list', lazy=True)
+
+  def __repr__(self):
+    return f'<TodoList {self.id} {self.name}>'
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
+      return f'<Todo {self.id} {self.description}, list {self.list_id}>'
+
 
 # db.create_all()
 body = {}
@@ -57,6 +68,8 @@ def create_todo():
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
+    body['id'] = todo.id
+    body['completed'] = todo.completed
     body['description'] = todo.description
   except:
     error = True
